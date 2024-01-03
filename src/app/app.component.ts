f97d3b3279a89296940d792b67c260b70fbe472f
Amir Asgari <Mr.a.asgari@gmail.com>
2024-01-03 18:59:20 +0330

    app.ts -- add some get data to show in screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular11App';
+  responseData: string | undefined;
+  responseId: string | undefined;
+  responseBody: string | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  sendHttpRequest() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe(
+      // (data: any) => {
+      ({title , id , body}: any) => {
+        this.responseData = title;
+        this.responseId = id;
+        this.responseBody = body;
+        // console.log(data);
+        
+      },
+      (error) => {
+        console.error('Error', error);
+      }
+    );
+  }
 }
