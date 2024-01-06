ebcfbd4351c1c0d196d35208d96da752c22b3af1
Amir Asgari <Mr.a.asgari@gmail.com>
2024-01-07 01:24:35 +0330

    clean the git

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,7 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+  responseData: string | undefined;
+
+  constructor(private http: HttpClient) { }
+  sendHttpRequest() {
+    // اینجا لاگین به سرور تستی را انجام دهید
+    // اطلاعات نام کاربری و رمز را به عنوان پارامترها به سرور ارسال کنید
+    // سپس پاسخ را در متغیر responseData ذخیره کنید
+    // مثال:
+    this.http.post('آدرس_سرور_تستی', { username: this.username, password: this.password })
+      .subscribe(
+        (data: any) => {
+          this.responseData = data.message; // در اینجا ممکن است پیام خاصی از سرور دریافت کنید
+        },
+        (error) => {
+          console.error('Error', error);
+        }
+      );
+  }
 
   ngOnInit(): void {
   }
